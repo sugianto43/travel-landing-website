3d47c4931717b57ba3b8e2a9fd30a57e96e63181
sugianto43 <antho9922@gmail.com>
2021-05-28 18:29:47 +0800

    add home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import "../../App.css";
+import Card from "../Card";
+import HeroSection from "../HeroSection";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Card />
+    </>
+  );
+}
+
+export default Home;
